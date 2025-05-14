95608ccaf3cf2106b15f9cddb563a381b15d757b
Md. Golam Musabbir Joy <45542331+Gomuj@users.noreply.github.com>
2025-05-14 22:05:28 +0600

    Update logout.php

--- hotel_db/logout.php
+++ hotel_db/logout.php
@@ -1,9 +1,23 @@
 <?php
-// logout.php
-// include 'dbconnect.php';
 session_start();
 session_unset();
 session_destroy();
-header("Location: login.php");
-exit;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="2;url=login.html">
+  <title>Logging Out</title>
+  <link rel="stylesheet" href="CSS/style.css">
+  <style>
+    
+  </style>
+</head>
+<body>
+  <div class="logout-container">
+    <h2>Logging You Out...</h2>
+    <p>You will be redirected to the login page shortly.</p>
+  </div>
+</body>
+</html>
