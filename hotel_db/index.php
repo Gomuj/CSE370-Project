e26a990e002ed7c9150b3d03b9e65eca1ca1eb23
Md. Golam Musabbir Joy <45542331+Gomuj@users.noreply.github.com>
2025-05-14 22:02:50 +0600

    Update index.php

--- hotel_db/index.php
+++ hotel_db/index.php
@@ -7,7 +7,12 @@
   <title>Hotel Home</title>
 </head>
 <body>
-  <h2>Welcome to Our Hotel</h2>
-  <a href="signup.php">Sign Up</a> | <a href="login.php">Login</a>
+  <div class="index-container">
+  <h2 class="title">Welcome to Our Hotel</h2>
+  <div class="button-group">
+  <a href="register.php" class="index-btn">Register</a>
+  <a href="login.php" class="index-btn">Login</a>
+  </div>
+  </div>
 </body>
 </html>
